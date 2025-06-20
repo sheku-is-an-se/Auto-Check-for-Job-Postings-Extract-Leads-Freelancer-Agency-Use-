12be740317838eb43a6635a47720f48a5b0122d4
John Doe <johndoe@email.com>
2025-06-20 14:40:40 -0400

    I added the correct location(us) and started writing code for salary(User driver seat coming soon)

--- scraper/remoteok_scraper.py
+++ scraper/remoteok_scraper.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 import time
 
 
@@ -12,8 +14,19 @@ import time
 #Set up Chromedriver
 driver = webdriver.Chrome()
 
+#atempt to put browser in full screen
+driver.maximize_window()
+
 #Navigate to remoteok
 driver.get("https://remoteok.com/")
+
+#Scroll to element
+iframe = driver.find_element(By.CLASS_NAME, "box")
+scroll_origin = ScrollOrigin.from_element(iframe)
+ActionChains(driver)\
+    .scroll_from_origin(scroll_origin, 0, 300)\
+    .perform()
+
 
 
 
@@ -24,36 +37,16 @@ location_button.click()
 time.sleep(1)
 print(driver.find_element(By.TAG_NAME, "tr").get_attribute('data-offset') == '3')
 
-'''
-iframe = driver.find_element(By.TAG_NAME, "tr").get_attribute('3')
-ActionChains(driver)\
-    .scroll_to_element(iframe)\
-    .perform()
-'''
+
 location_dropdown = driver.find_element(By.CLASS_NAME, "location-filter-results").find_elements(By.TAG_NAME, "div")
 
 for element in location_dropdown:
-    if element.text == "🌏 Worldwide":
+    if element.text == "🇺🇸 United States":
         print('I found the US')
         element.click()
-        time.sleep(3)
+        time.sleep(1)
         break
    
-    
-
-
-
-
-
-#Locate the United STates tag
-
-
-
-
-
-#for location in location_dropdown:
-    #print(location.text)
-
 
 
 
@@ -67,24 +60,46 @@ search_box.send_keys("Python")
 
 #Enter Result in search box
 search_box.send_keys(Keys.RETURN)
-time.sleep(3)
+time.sleep(1)
+
+#Salary( MINIMUM )
+#Change the number (0-100)k
+salary_min_box = driver.find_element(By.CLASS_NAME, "salary-filter")
+salary_min_box.click()
+
+#click and hold slider to 50
+clickable = driver.find_element(By.CLASS_NAME, "salary-filter-input")
+ActionChains(driver) \
+        .click(clickable) \
+        .perform()
+time.sleep(1)
+
+#UNCOMMENT AND REPLACE CLICK AND HOLD SLIDER TO 50. THE PLACEHOLDER IS FOR TESTING PURPOSED AS THE MOMEMNT
+'''
+draggable = driver.find_element(By.ID, "draggable")
+start = draggable.location
+finish = driver.find_element(By.ID, "droppable").location
+ActionChains(driver) \
+    .drag_and_drop_by_offset(draggable, finish['x'] - start['x'], finish['y'] - start['y']) \
+    .perform()
+'''
 
 
-
-
-
-#cRetrieve the page source
+#Retrieve the page source
 html = driver.page_source
 
 # Parse the HTML with Beautiful Soup
 soup = BeautifulSoup(html, 'lxml')
 
-#print(soup.prettify())
+soupdata = print(soup.prettify())
 
+#---------------------------------------
+##############USER DRIVER SEAT############
+##############UNCOMMENT TO SET PREFERENCES(OPTIONAL)###############
+#---------------------------------------
 
 #close the browser
 driver.quit()
 
 
-#search box element
-#class="search active"
+#I need to click and hold the slider from left to right and pick what salary i want. after that, i'll create an easy to use dictionary holding the values of the slider from 0-25; I cvould just use javascript but this is a python project and im not too proficient in javascript anyways
