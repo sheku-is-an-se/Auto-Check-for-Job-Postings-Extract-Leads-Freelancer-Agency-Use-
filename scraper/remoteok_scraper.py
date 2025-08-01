a131327d1a8ddbacd5ae63c9e07b32a3eaf9e6e9
sheku-is-a-se <sheku2026@gmail.com>
2025-08-01 10:17:58 -0400

    You can now adjust your salary. Also, working on turning this project into a real world usecase. thank you

--- scraper/remoteok_scraper.py
+++ scraper/remoteok_scraper.py
@@ -24,9 +24,9 @@ driver.get("https://remoteok.com/")
 iframe = driver.find_element(By.CLASS_NAME, "box")
 scroll_origin = ScrollOrigin.from_element(iframe)
 ActionChains(driver)\
-    .scroll_from_origin(scroll_origin, 0, 300)\
+    .scroll_from_origin(scroll_origin, 0, 500)\
     .perform()
-
+time.sleep(1)
 
 
 
@@ -62,29 +62,81 @@ search_box.send_keys("Python")
 search_box.send_keys(Keys.RETURN)
 time.sleep(1)
 
-#Salary( MINIMUM )
-#Change the number (0-100)k
+#Salary SLIDER (Make sure to delete the salary code below to set your salary preferences)
 salary_min_box = driver.find_element(By.CLASS_NAME, "salary-filter")
 salary_min_box.click()
+time.sleep(1)
 
-#click and hold slider to 50
+'''
 clickable = driver.find_element(By.CLASS_NAME, "salary-filter-input")
 ActionChains(driver) \
         .click(clickable) \
         .perform()
 time.sleep(1)
-
-#UNCOMMENT AND REPLACE CLICK AND HOLD SLIDER TO 50. THE PLACEHOLDER IS FOR TESTING PURPOSED AS THE MOMEMNT
 '''
-draggable = driver.find_element(By.ID, "draggable")
-start = draggable.location
-finish = driver.find_element(By.ID, "droppable").location
-ActionChains(driver) \
-    .drag_and_drop_by_offset(draggable, finish['x'] - start['x'], finish['y'] - start['y']) \
-    .perform()
+draggable = driver.find_element(By.CLASS_NAME, "salary-filter-input")
+actions = ActionChains(driver) 
+actions.drag_and_drop_by_offset(draggable,-20, 0).perform()
+time.sleep(3)
+#------------------------------------------
+#---------------------------------------
+##############USER DRIVER SEAT############
+##############UNCOMMENT TO SET SALARY PREFERENCES(OPTIONAL)###############
 '''
+#Click salary button
+salary_min_box = driver.find_element(By.CLASS_NAME, "salary-filter")
+salary_min_box.click()
+
+slider_ranging_down_from_middle= {
+    120000: 0,
+    110000: -10,
+    100000: -20,
+    90000: -30,
+    80000: -40,
+    70000: -50,
+    60000: -60,
+    50000: -70,
+    40000: -80,
+    30000: -90,
+    20000: -100,
+    10000: -110,
+    0: -120
+}
+slider_ranging_up_from_middle = {
+    120000: 0,
+    140000: 10,
+    150000: 20,
+    160000: 30,
+    170000: 40,
+    180000: 50,
+    200000: 60,
+    210000: 70,
+    220000: 80,
+    230000: 90,
+    240000: 100,
+    250000: 120
+}
 
 
+#From 0-250000, please input the salary you want to set the slider to
+
+def set_slider_value(value):
+    if value in slider_ranging_up_from_middle:
+        slider_value = slider_ranging_up_from_middle[value]
+    elif value in slider_ranging_down_from_middle:
+        slider_value = slider_ranging_down_from_middle[value]
+    else:
+        raise ValueError("You did not choose a number between 0-250000")
+    return slider_value
+
+slider_value = set_slider_value() #Change this value to set the slider to your desired salary
+    
+draggable = driver.find_element(By.CLASS_NAME, "salary-filter-input")
+actions = ActionChains(driver) 
+actions.drag_and_drop_by_offset(draggable,slider_value, 0).perform()
+time.sleep(3)
+'''
+    
 #Retrieve the page source
 html = driver.page_source
 
@@ -93,13 +145,13 @@ soup = BeautifulSoup(html, 'lxml')
 
 soupdata = print(soup.prettify())
 
-#---------------------------------------
-##############USER DRIVER SEAT############
-##############UNCOMMENT TO SET PREFERENCES(OPTIONAL)###############
-#---------------------------------------
 
 #close the browser
 driver.quit()
 
 
 #I need to click and hold the slider from left to right and pick what salary i want. after that, i'll create an easy to use dictionary holding the values of the slider from 0-25; I cvould just use javascript but this is a python project and im not too proficient in javascript anyways
+#Requirements
+#simulate a click, hold and drag slider
+#move it to my preferences and save
+#save values and  compartmentalize
